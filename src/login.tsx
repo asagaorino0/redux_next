3dc1993775ba489668717cf3f2a2131858431bdd
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-12-04 09:59:38 +0900

    [add]LINEログインしたい

--- src/login.tsx
+++ src/login.tsx
@@ -127,11 +127,11 @@ const Login: NextPage = () => {
         <div className="h-screen w-4/5 max-w-5xl mx-auto flex items-center justifycenter flex-col">
             <button onClick={lineClick}>
                 <h3 className="mb-4 text-green-500 text-3xl">ログイン</h3></button>
-            <a href="https://access.line.me/oauth2/v2.1/authorize?app_id=1656650515-ENMoxvjb&client_id=1656650515&scope=chat_message.write+openid+profile&state=MTSFhIGGxsff&bot_prompt=aggressive&response_type=code&code_challenge_method=S256&code_challenge=Hx-YFyPAvO9ZQIg5pQpaGQuMChsOE11Raf_3DHDGFgY&liff_sdk_version=2.11.1&type=L&redirect_uri=http://localhost:3000">
-                <div>
-                    ログイン
-                </div>
-            </a>
+            {/* <a href="https://access.line.me/oauth2/v2.1/authorize?app_id=1656650515-ENMoxvjb&client_id=1656650515&scope=chat_message.write+openid+profile&state=MTSFhIGGxsff&bot_prompt=aggressive&response_type=code&code_challenge_method=S256&code_challenge=Hx-YFyPAvO9ZQIg5pQpaGQuMChsOE11Raf_3DHDGFgY&liff_sdk_version=2.11.1&type=L&redirect_uri=http://localhost:3000"> */}
+            <div>
+                {name}
+            </div>
+            {/* </a> */}
         </div>
     )
 }
