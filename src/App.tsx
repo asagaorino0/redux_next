7b77d498d541d89a5a0f203e41d62487736d5588
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-12-04 12:14:20 +0900

    [add]LINEログインしたいApp

--- src/App.tsx
+++ src/App.tsx
@@ -30,45 +30,83 @@ function App() {
   // const LINEID = "1656149559-xXM4l4Gp"
   // const loginUrl = "https://access.line.me/oauth2/v2.1/authorize?app_id=1656650515-ENMoxvjb&client_id=1656650515&scope=chat_message.write+openid+profile&state=MTSFhIGGxsff&bot_prompt=aggressive&response_type=code&code_challenge_method=S256&code_challenge=Hx-YFyPAvO9ZQIg5pQpaGQuMChsOE11Raf_3DHDGFgY&liff_sdk_version=2.11.1&type=L&redirect_uri=http://localhost:3000/"
   console.log('LINEID', LINEID)
-  const lineClick = () => {
-    liff.init({ liffId: LINEID as string }) // LIFF IDをセットする
+  // const lineClick = () => {
+  //   liff.init({ liffId: LINEID as string }) // LIFF IDをセットする
+  //     .then(() => {
+  //       if (!liff.isLoggedIn()) {
+  //         liff.login({}) // ログインしていなければ最初にログインする
+  //       } else if (liff.isInClient()) { // LIFFので動いているのであれば
+  //         liff.sendMessages([{ // メッセージを送信する
+  //           'type': 'text',
+  //           'text': "You've successfully sent a message! Hooray!"
+  //         }]).then(function () {
+  //           window.alert('Message sent');
+  //         }).catch(function (error) {
+  //           window.alert('Error sending message: ' + error);
+  //         });
+  //       }
+  //     })
+  // }
+  // const onload = () => {
+  //   liff.init({ liffId: process.env.NEXT_PUBLIC_REACT_APP_LIFF_ID as string })
+  //     .then(() => {
+  //       if (!liff.isLoggedIn()) {
+  //         liff.login({}) // ログインしていなければ最初にログインする
+  //       } else if (liff.isInClient()) {
+  //         liff.getProfile()  // ユーザ情報を取得する
+  //           .then(profile => {
+  //             const userId: string = profile.userId
+  //             const displayName: string = profile.displayName
+  //             setName(profile.displayName)
+  //             // setUid(profile.userId)
+  //             console.log("{login}", `${name}`);
+  //             registUser()
+  //             alert(`Name: ${displayName}, userId: ${userId}`)
+  //           }).catch(function (error) {
+  //             window.alert('Error sending message: ' + error);
+  //           });
+  //       }
+  //     })
+  // }
+  const lineClick = function () {
+    onload()
+    // liff.login();
+    // window.location.href = loginUrl;
+  };
+  const onload = function () {
+    liff
+      .init({ liffId: LINEID as string })
       .then(() => {
-        if (!liff.isLoggedIn()) {
-          liff.login({}) // ログインしていなければ最初にログインする
-        } else if (liff.isInClient()) { // LIFFので動いているのであれば
-          liff.sendMessages([{ // メッセージを送信する
-            'type': 'text',
-            'text': "You've successfully sent a message! Hooray!"
-          }]).then(function () {
-            window.alert('Message sent');
+        // 初期化完了
+        initializeApp();
+        liff.getProfile()  // ユーザ情報を取得する
+          .then(profile => {
+            const userId: string = profile.userId
+            const displayName: string = profile.displayName
+            setName(profile.displayName)
+            // setUid(profile.userId)
+            console.log("{login}", `${name}`);
+            alert(`Name: ${displayName}, userId: ${userId}`)
           }).catch(function (error) {
             window.alert('Error sending message: ' + error);
           });
-        }
       })
+  };
+  function initializeApp() {
+    // ログインチェック
+    if (liff.isLoggedIn()) {
+      //ログイン済
+      // onload()
+      onload()
+    } else {
+      // 未ログイン
+      // let result = window.confirm("LINE Loginしますか？新着情報を確認する場合はキャンセルしてください。");
+      // if (result) {
+      liff.login();
+      // window.location.href = loginUrl;
+      // }
+    }
   }
-  const onload = () => {
-    liff.init({ liffId: process.env.NEXT_PUBLIC_REACT_APP_LIFF_ID as string })
-      .then(() => {
-        if (!liff.isLoggedIn()) {
-          liff.login({}) // ログインしていなければ最初にログインする
-        } else if (liff.isInClient()) {
-          liff.getProfile()  // ユーザ情報を取得する
-            .then(profile => {
-              const userId: string = profile.userId
-              const displayName: string = profile.displayName
-              setName(profile.displayName)
-              // setUid(profile.userId)
-              console.log("{login}", `${name}`);
-              registUser()
-              alert(`Name: ${displayName}, userId: ${userId}`)
-            }).catch(function (error) {
-              window.alert('Error sending message: ' + error);
-            });
-        }
-      })
-  }
-
   return (
     <div className="App">
       <h1>name</h1>
