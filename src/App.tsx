d10c5714a429944a6a3faf07d83093f6c89aba61
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-12-05 21:07:07 +0900

    [add]LINEliffとfirebase

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ function App() {
       .then(() => {
         if (!liff.isLoggedIn()) {
           liff.login({}) // ログインしていなければ最初にログインする
-          // router.push('https://access.line.me/oauth2/v2.1/authorize?app_id=1656149559-xXM4l4Gp&client_id=1656149559&scope=chat_message.write+openid+profile&state=MTSFhIGGxsff&bot_prompt=aggressive&response_type=code&code_challenge_method=S256&code_challenge=Hx-YFyPAvO9ZQIg5pQpaGQuMChsOE11Raf_3DHDGFgY&liff_sdk_version=2.11.1&type=L&redirect_uri=https://redux-next.vercel.app/');
+          // router.push();
         } else if (liff.isInClient()) {
           liff.getProfile()  // ユーザ情報を取得する
             .then(profile => {
@@ -160,13 +160,12 @@ function App() {
           <h1 className="mb-4 text-green-500 text-3xl">ようこそ </h1>
         </button>
       }
-      <a href={loginUrl}>
+      <a href=' https://access.line.me/oauth2/v2.1/authorize?app_id=1656149559-xXM4l4Gp&client_id=1656149559&scope=chat_message.write+openid+profile&state=MTSFhIGGxsff&bot_prompt=aggressive&response_type=code&code_challenge_method=S256&code_challenge=Hx-YFyPAvO9ZQIg5pQpaGQuMChsOE11Raf_3DHDGFgY&liff_sdk_version=2.11.1&type=L&redirect_uri=https://redux-next.vercel.app/'>
         <div>
           <h4 className="mb-4 text-green-500 text-3xl">ログインはここをタップ</h4>
         </div>
       </a>
-      {/* <PageA /> */}
-    </div>
+    </div >
   );
 }
 
