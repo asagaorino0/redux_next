8ae7cae1791abd3b1143baaa808bb00b60334ce1
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-12-05 12:46:21 +0900

    [add]LINEliffとfirebase

--- src/App.tsx
+++ src/App.tsx
@@ -35,32 +35,33 @@ function App() {
   };
   const loginUrl: string | undefined = process.env.NEXT_PUBLIC_LINE_LOGIN_URL
   const LINEID = process.env.NEXT_PUBLIC_REACT_APP_LIFF_ID
-  // const lineClick = () => {
-  //   liff.init({ liffId: LINEID as string }) // LIFF IDをセットする
-  //     .then(() => {
-  //       if (!liff.isLoggedIn()) {
-  //         liff.login({}) // ログインしていなければ最初にログインする
-  //       } else if (liff.isInClient()) {
-  //         liff.getProfile()  // ユーザ情報を取得する
-  //           .then(profile => {
-  //             const userId: string = profile.userId
-  //             const displayName: string = profile.displayName
-  //             const displayicon: string | undefined = profile.pictureUrl
-  //             setName(profile.displayName)
-  //             setUid(userId)
-  //             setName(displayName)
-  //             setIcon(displayicon)
-  //             dispatch(addUser({ name, uid, icon }))
-  //             // let result = window.confirm(`Name1: ${displayName}さん、ログインします。`);
-  //             // // alert(`Name1: ${displayName}, userId: ${userId}`)
-  //             // if (result) {
-  //             onload()
-  //           }).catch(function (error) {
-  //             window.alert('Error sending message: ' + error);
-  //           });
-  //       }
-  //     })
-  // }
+  const lineClick = () => {
+    liff.init({ liffId: LINEID as string }) // LIFF IDをセットする
+      .then(() => {
+        if (!liff.isLoggedIn()) {
+          liff.login({}) // ログインしていなければ最初にログインする
+          router.push('https://access.line.me/oauth2/v2.1/authorize?app_id=1656149559-xXM4l4Gp&client_id=1656149559&scope=chat_message.write+openid+profile&state=MTSFhIGGxsff&bot_prompt=aggressive&response_type=code&code_challenge_method=S256&code_challenge=Hx-YFyPAvO9ZQIg5pQpaGQuMChsOE11Raf_3DHDGFgY&liff_sdk_version=2.11.1&type=L&redirect_uri=https://redux-next.vercel.app/');
+        } else if (liff.isInClient()) {
+          liff.getProfile()  // ユーザ情報を取得する
+            .then(profile => {
+              const userId: string = profile.userId
+              const displayName: string = profile.displayName
+              const displayicon: string | undefined = profile.pictureUrl
+              setName(profile.displayName)
+              setUid(userId)
+              setName(displayName)
+              setIcon(displayicon)
+              dispatch(addUser({ name, uid, icon }))
+              // let result = window.confirm(`Name1: ${displayName}さん、ログインします。`);
+              // // alert(`Name1: ${displayName}, userId: ${userId}`)
+              // if (result) {
+              onload()
+            }).catch(function (error) {
+              window.alert('Error sending message: ' + error);
+            });
+        }
+      })
+  }
   // const lineClick = function () {
   //   liff
   //     .init({ liffId: LINEID as string })
@@ -142,17 +143,17 @@ function App() {
   //   }
   // }, []
   // );
-  const lineClick = () => {
-    liff.init({ liffId: LINEID as string }) // LIFF IDをセットする
-      .then(() => {
-        if (!liff.isLoggedIn()) {
-          // liff.login({}) // ログインしていなければ最初にログインする
-          router.push('https://access.line.me/oauth2/v2.1/authorize?app_id=1656149559-xXM4l4Gp&client_id=1656149559&scope=chat_message.write+openid+profile&state=MTSFhIGGxsff&bot_prompt=aggressive&response_type=code&code_challenge_method=S256&code_challenge=Hx-YFyPAvO9ZQIg5pQpaGQuMChsOE11Raf_3DHDGFgY&liff_sdk_version=2.11.1&type=L&redirect_uri=https://redux-next.vercel.app/');
-        } else {
-          onload()
-        }
-      })
-  }
+  // const lineClick = () => {
+  //   liff.init({ liffId: LINEID as string }) // LIFF IDをセットする
+  //     .then(() => {
+  //       if (!liff.isLoggedIn()) {
+  //         // liff.login({}) // ログインしていなければ最初にログインする
+  //         router.push('https://access.line.me/oauth2/v2.1/authorize?app_id=1656149559-xXM4l4Gp&client_id=1656149559&scope=chat_message.write+openid+profile&state=MTSFhIGGxsff&bot_prompt=aggressive&response_type=code&code_challenge_method=S256&code_challenge=Hx-YFyPAvO9ZQIg5pQpaGQuMChsOE11Raf_3DHDGFgY&liff_sdk_version=2.11.1&type=L&redirect_uri=https://redux-next.vercel.app/');
+  //       } else {
+  //         onload()
+  //       }
+  //     })
+  // }
 
   return (
     <div className="App">
