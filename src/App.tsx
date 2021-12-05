6038a65beaddfeebc4691a8d950b9e7f155a030b
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-12-05 12:59:08 +0900

    [add]LINEliffとfirebase

--- src/App.tsx
+++ src/App.tsx
@@ -39,8 +39,8 @@ function App() {
     liff.init({ liffId: LINEID as string }) // LIFF IDをセットする
       .then(() => {
         if (!liff.isLoggedIn()) {
-          // liff.login({}) // ログインしていなければ最初にログインする
-          router.push('https://access.line.me/oauth2/v2.1/authorize?app_id=1656149559-xXM4l4Gp&client_id=1656149559&scope=chat_message.write+openid+profile&state=MTSFhIGGxsff&bot_prompt=aggressive&response_type=code&code_challenge_method=S256&code_challenge=Hx-YFyPAvO9ZQIg5pQpaGQuMChsOE11Raf_3DHDGFgY&liff_sdk_version=2.11.1&type=L&redirect_uri=https://redux-next.vercel.app/');
+          liff.login({}) // ログインしていなければ最初にログインする
+          // router.push('https://access.line.me/oauth2/v2.1/authorize?app_id=1656149559-xXM4l4Gp&client_id=1656149559&scope=chat_message.write+openid+profile&state=MTSFhIGGxsff&bot_prompt=aggressive&response_type=code&code_challenge_method=S256&code_challenge=Hx-YFyPAvO9ZQIg5pQpaGQuMChsOE11Raf_3DHDGFgY&liff_sdk_version=2.11.1&type=L&redirect_uri=https://redux-next.vercel.app/');
         } else if (liff.isInClient()) {
           liff.getProfile()  // ユーザ情報を取得する
             .then(profile => {
