0f00e61ec136bfbfbf108dfde2145a0ef4dd5a2d
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-12-12 19:31:29 +0900

    [add]api_main

--- pages/api/[name].tsx
+++ pages/api/[name].tsx
@@ -4,22 +4,22 @@ import useSWR from 'swr'
 const fetcher = async (url: string) => {
 
     const res = await fetch('/api/')
-    const data = await res.json()
-    if (res.status !== 200) {
-        throw new Error(data.message)
-    }
-    return data
+    // const data = await res.json()
+    // if (res.status !== 200) {
+    //     throw new Error(data.message)
+    // }
+    // return data
 }
 // export const Person = async (url: string) => {
 export default function User() {
     const { query } = useRouter()
-    const { data, error } = useSWR(
-        () => query.id && `/api/${query.name}`,
-        fetcher
-    )
-    console.log(data)
-    if (error) return <div>{error.message}</div>
-    if (!data) return <div>Loading...</div>
+    // const { data, error } = useSWR(
+    //     () => query.id && `/api/${query.name}`,
+    //     fetcher
+    // )
+    // console.log(data)
+    // if (error) return <div>{error.message}</div>
+    // if (!data) return <div>Loading...</div>
     // console.log(data)
 
     return (
@@ -28,37 +28,12 @@ export default function User() {
                 <tr>
                     <th></th>
                     <th>Name</th>
-                    {/* <th>Height</th>
-                    <th>Mass</th>
-                    <th>Hair color</th>
-                    <th>Skin color</th>
-                    <th>Eye color</th>
-                    <th>Gender</th> */}
                 </tr>
             </thead>
             <tbody>
-                {/* <tr> */}
-                {/* <td>{query.id}</td>
-                    <td>{data.name}</td> */}
-                {/* <td>{data.height}</td>
-                    <td>{data.mass}</td>
-                    <td>{data.hair_color}</td>
-                    <td>{data.skin_color}</td>
-                    <td>{data.eye_color}</td>
-                    <td>{data.gender}</td> */}
-                {
-                    data
-                        .filter((data: any) => data.uid === query.id)
-                        .map((data: any) => {
-                            return (
-                                <tr key={data.uid}>
-                                    <td>
-                                        {data.name}
-                                    </td>
-                                </tr>
-                            )
-                        })
-                }
+                <tr>
+                    <td>{query.name}</td>
+                </tr>
             </tbody>
         </table>
     )
