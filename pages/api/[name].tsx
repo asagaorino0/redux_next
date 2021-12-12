79cb8a07b02809fcdce9a24b6bf1f6f6e44275ac
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-12-12 20:47:33 +0900

    [add]api_main

--- pages/api/[name].tsx
+++ pages/api/[name].tsx
@@ -1,43 +1,13 @@
-import { useRouter } from 'next/router'
-import useSWR from 'swr'
-// import type { NextApiRequest, NextApiResponse } from 'next'
-// export default function handler(req: NextApiRequest, res: NextApiResponse) {
-//     res.status(200).json(name)
-// export default (query: { name: string }, res: NextApiResponse) => {
-//     res.status(200).json({ message: `you requested for ${query.name} ` });
-// };
+import { users } from '../../src/data'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-const fetcher = async (url: string) => {
-    const res = await fetch('/api/')
-    const data = await res.json()
-    if (res.status !== 200) {
-        throw new Error(data.message)
+export default function personHandler(query: { name: string }, res: NextApiResponse) {
+    const filtered = users.filter((p: any) => p.name === query.name)
+
+    // User with id exists
+    if (filtered.length > 0) {
+        res.status(200).json(filtered[0])
+    } else {
+        res.status(404).json({ message: `User with id: ${query.name} not found.` })
     }
-    return data
-}
-export default function Name() {
-    const { query } = useRouter()
-    const { data, error } = useSWR(
-        () => query.id && `/api/${query.name}`,
-        fetcher
-    )
-    console.log(data)
-    if (error) return <div>{error.message}</div>
-    if (!data) return <div>Loading...</div>
-    // console.log(data)
-
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>{query.name}</td>
-                </tr>
-            </tbody>
-        </table>
-    )
 }
