2dbab1bdbf13d769336cf46a87dd6b7ee74403d7
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-12-13 21:57:36 +0900

    [add]api_main

--- pages/api/users/[id].tsx
+++ pages/api/users/[id].tsx
@@ -2,12 +2,12 @@ import { UsersItem } from '../../../src/data'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default function personHandler(query: { id: string }, res: NextApiResponse) {
-    const filtered = UsersItem.filter((p: any) => p.id === query.id)
+    // const filtered = UsersItem.filter((p: any) => p.id === query.id)
 
     // User with id exists
-    if (filtered.length > 0) {
-        res.status(200).json(filtered[0])
-    } else {
-        res.status(404).json({ message: `User with id: ${query.id} not found.` })
-    }
+    // if (filtered.length > 0) {
+    res.status(200).json(query.id)
+    // } else {
+    // res.status(404).json({ message: `User with id: ${query.id} not found.` })
 }
+// }
