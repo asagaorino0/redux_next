e0a9bbcc9af5a3f09becbdc40c6b596d028a7c45
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-12-13 23:30:34 +0900

    [add]api_main帰ってきた

--- src/data.tsx
+++ src/data.tsx
@@ -9,11 +9,11 @@ console.log('useStete:', user)
 
 export const users = [
     {
-        uid: user.uid,
-        name: user.name,
+        uid: 'user.uid',
+        name: 'user.name',
     },
     {
-        uid: 'Uda1c6a4e5b348c5ba3c95de639e32415',
+        uid: 'Uda1c6a4e5b348c5ba3c95de639e32414',
         name: 'えりこ',
     },
     {
