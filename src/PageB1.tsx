d10bf24ebccc2470963e7a834eb0b30b0ad95610
asagaorino0 <xcnsn724@yahoo.co.jp>
2022-01-14 09:43:39 +0900

    [add]予約枠

--- src/PageB1.tsx
+++ src/PageB1.tsx
@@ -47,132 +47,132 @@ const PageB1 = () => {
     //         console.log('name:', user.namae)
     //     })
     // }, []);
-    // // const registYoyaku = () => {
-    // //     const addRef = addDoc(collection(db, 'yoyaku', `${user.uid}`, 'ukeru', `${gappi}oo`), {
-    // //         sei,
-    // //         menu,
-    // //         uid: `${user.uid}`,
-    // //         namae: namae,
-    // //         tokoro,
-    // //         ukeruId: `${gappi}oo`,
-    // //         timestamp: Timestamp.fromDate(new Date()),
-    // //     })
-    // //     const setRef = setDoc(doc(db, 'users', `${user.uid}`, 'tomare', `${gappi}oo`), {
-    // //         gappi,
-    // //         menu,
-    // //         cUid: `${user.uid}`,
-    // //         namae: namae,
-    // //         tokoro,
-    // //         ukeruId: `${gappi}oo`,
-    // //         timestamp: Timestamp.fromDate(new Date()),
-    // //     }, { merge: true }//←上書きされないおまじない
-    // //     )
-    // //     console.log('ukeru:', addRef)
-    // //     console.log('tomare:', setRef)
-    // //     alert('登録しました。ありがとうございます。')
-    // // };
-    const [users, setUsers] = useState<any>();
-    const [tomare, setTomare] = useState<any>([]);
-    const [tomareId, setTomareId] = useState<any>([]);
-    // const uid = "Uda1c6a4e5b348c5ba3c95de639e32414"
-    const registUser = () => {
-        dispatch(addUser({
-            users,
-        }))
+    const registYoyaku = () => {
+        const addRef = addDoc(collection(db, 'yoyaku', `${user.uid}`, 'ukeru', `${gappi}oo`), {
+            sei,
+            menu,
+            uid: `${user.uid}`,
+            namae: namae,
+            tokoro,
+            ukeruId: `${gappi}oo`,
+            timestamp: Timestamp.fromDate(new Date()),
+        })
+        // //     const setRef = setDoc(doc(db, 'users', `${user.uid}`, 'tomare', `${gappi}oo`), {
+        // //         gappi,
+        // //         menu,
+        // //         cUid: `${user.uid}`,
+        // //         namae: namae,
+        // //         tokoro,
+        // //         ukeruId: `${gappi}oo`,
+        // //         timestamp: Timestamp.fromDate(new Date()),
+        // //     }, { merge: true }//←上書きされないおまじない
+        // //     )
+        // //     console.log('ukeru:', addRef)
+        // //     console.log('tomare:', setRef)
+        // //     alert('登録しました。ありがとうございます。')
+        // // };
+        const [users, setUsers] = useState<any>();
+        const [tomare, setTomare] = useState<any>([]);
+        const [tomareId, setTomareId] = useState<any>([]);
+        // const uid = "Uda1c6a4e5b348c5ba3c95de639e32414"
+        const registUser = () => {
+            dispatch(addUser({
+                users,
+            }))
+        }
+
+        useEffect(() => {
+            // let users: any = []
+            const fetchUsers = async () => {
+                const querrySnapshot = await getDocs(collection(db, 'users'))
+                const usersData = querrySnapshot.docs.map(
+                    (doc: any) => ({ ...doc.data() } as UserState))
+                setUsers(usersData)
+                // setUid(users.uid)
+                console.log('usersData:', usersData)
+                console.log('users:', users)
+                registUser()
+            }
+            fetchUsers()
+            // registUser()
+            console.log('users:', users)
+            // fetchTomare(uid)
+        }, []);
+        // useEffect(() => {
+        //     const fetchMenus = async () => {
+        //         const q = query(collection(db, 'users'), where("uid", "==", user.uid));
+        //         const snapshot = await getDocs(q)
+        //         const menuData = snapshot.docs.map(
+        //             (doc: any) => ({ ...doc.data().menu }))
+        //         console.log('usersData:', menuData)
+        //         dispatch(addMenu(menuData))
+        //         setMenus(menuData)
+        //         console.log('menus:', menus)
+        //     }
+        //     fetchMenus()
+        //     console.log('menus:', menus)
+        // }, []);
+
+
+        return (
+            <div className="App">
+                <span >pageB1：登録</span>
+                <br />
+                {`${user.icon}`.length !== 0 &&
+                    <img
+                        src={`${user.icon}`}
+                        alt=""
+                        style={{ borderRadius: '50%', width: '60px', height: '60px' }}
+                    />
+                }
+                {`${user.icon}`.length !== 0 &&
+                    <h1 className="mb-4 text-green-500 text-3xl">{user.name}さま </h1>
+                }
+                <h1>氏名</h1>
+                {/* {users.namae} */}
+                <input type="text" onChange={(e) => setNamae(e.target.value)} />
+                <br />
+                <h1>性別</h1>
+                <input type="text" onChange={(e) => setSei(e.target.value)} />
+                <br />
+                <h1>住所</h1>
+                <input type="text" onChange={(e) => setTokoro(e.target.value)} />
+                <br />
+                ************************************************
+                <br />
+                {/* <div > */}
+                {/* {users.length !== 0 && */}
+                {
+                    // users
+                    //     .map((data: any) => {
+                    //         return (
+                    //             <div key={data.uid}>
+                    //                 <div>
+                    //                     <img
+                    //                         src={`${data.icon}`}
+                    //                         alt=""
+                    //                         style={{ borderRadius: '50%', width: '60px', height: '60px' }}
+                    //                     />
+                    //                 </div>
+                    //                 <h1>氏名（屋号）</h1>
+                    //                 {data.name}
+                    //                 <div>
+                    //                 </div>
+                    //                 {/* <React.StrictMode > */}
+                    //                 {/* <Provider store={store}> */}
+                    //                 {/* <CalendarPage tomareId={data.uid} key={data.uid} /> */}
+                    //                 {/* </Provider> */}
+                    //                 {/* </React.StrictMode> */}
+                    //                 {/* <CalendarPage tomareId={tomareId} key={tomare.uid} /> */}
+                    //                 <br />
+                    <button onClick={registYoyaku}>登録</button>
+                    //             </div >
+                    //         );
+                    //     })
+                }
+            </div >
+        )
     }
 
-    useEffect(() => {
-        // let users: any = []
-        const fetchUsers = async () => {
-            const querrySnapshot = await getDocs(collection(db, 'users'))
-            const usersData = querrySnapshot.docs.map(
-                (doc: any) => ({ ...doc.data() } as UserState))
-            setUsers(usersData)
-            // setUid(users.uid)
-            console.log('usersData:', usersData)
-            console.log('users:', users)
-            registUser()
-        }
-        fetchUsers()
-        // registUser()
-        console.log('users:', users)
-        // fetchTomare(uid)
-    }, []);
-    // useEffect(() => {
-    //     const fetchMenus = async () => {
-    //         const q = query(collection(db, 'users'), where("uid", "==", user.uid));
-    //         const snapshot = await getDocs(q)
-    //         const menuData = snapshot.docs.map(
-    //             (doc: any) => ({ ...doc.data().menu }))
-    //         console.log('usersData:', menuData)
-    //         dispatch(addMenu(menuData))
-    //         setMenus(menuData)
-    //         console.log('menus:', menus)
-    //     }
-    //     fetchMenus()
-    //     console.log('menus:', menus)
-    // }, []);
-
-
-    return (
-        <div className="App">
-            <span >pageB1：登録</span>
-            <br />
-            {`${user.icon}`.length !== 0 &&
-                <img
-                    src={`${user.icon}`}
-                    alt=""
-                    style={{ borderRadius: '50%', width: '60px', height: '60px' }}
-                />
-            }
-            {`${user.icon}`.length !== 0 &&
-                <h1 className="mb-4 text-green-500 text-3xl">{user.name}さま </h1>
-            }
-            <h1>氏名</h1>
-            {/* {users.namae} */}
-            <input type="text" onChange={(e) => setNamae(e.target.value)} />
-            <br />
-            <h1>性別</h1>
-            <input type="text" onChange={(e) => setSei(e.target.value)} />
-            <br />
-            <h1>住所</h1>
-            <input type="text" onChange={(e) => setTokoro(e.target.value)} />
-            <br />
-            ******************************************************
-            <br />
-            {/* <div > */}
-            {/* {users.length !== 0 && */}
-            {
-                // users
-                //     .map((data: any) => {
-                //         return (
-                //             <div key={data.uid}>
-                //                 <div>
-                //                     <img
-                //                         src={`${data.icon}`}
-                //                         alt=""
-                //                         style={{ borderRadius: '50%', width: '60px', height: '60px' }}
-                //                     />
-                //                 </div>
-                //                 <h1>氏名（屋号）</h1>
-                //                 {data.name}
-                //                 <div>
-                //                 </div>
-                //                 {/* <React.StrictMode > */}
-                //                 {/* <Provider store={store}> */}
-                //                 {/* <CalendarPage tomareId={data.uid} key={data.uid} /> */}
-                //                 {/* </Provider> */}
-                //                 {/* </React.StrictMode> */}
-                //                 {/* <CalendarPage tomareId={tomareId} key={tomare.uid} /> */}
-                //                 <br />
-                //                 {/* <button onClick={registYoyaku}>登録</button> */}
-                //             </div >
-                //         );
-                //     })
-            }
-        </div >
-    )
-}
-
-export default PageB1
+    export default PageB1
 
