3cce8beb662086dbabf73fd1ca14cd0be374ea0e
asagaorino0 <xcnsn724@yahoo.co.jp>
2022-02-05 23:02:30 +0900

    PageA_tomare表示／非表示

--- components/TomareAccordion.tsx
+++ components/TomareAccordion.tsx
@@ -70,16 +70,16 @@ export default function SimpleAccordion({ tomare }: { tomare: TomareState }) {
         (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
             setExpanded(newExpanded ? panel : false);
         };
-    const [checked, setChecked] = React.useState([tomare.checked]);
+    const [checked, setChecked] = React.useState([true, false]);
 
 
-    // const handleChange2 = (event: React.ChangeEvent<HTMLInputElement>) => {
-    //     setChecked([event.target.checked, checked[1]]);
-    //     console.log(checked)
-    //     setDoc(doc(db, 'users', user.uid, 'tomare', `${tomare.tomareId}`), {
-    //         checked: { checked },
-    //     }, { merge: true })
-    // };
+    const handleChange2 = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setChecked([event.target.checked, checked[1]]);
+        console.log(checked)
+        setDoc(doc(db, 'users', user.uid, 'tomare', `${tomare.tomareId}`), {
+            checked: { checked },
+        }, { merge: true })
+    };
 
     return (
         <div>
@@ -91,8 +91,7 @@ export default function SimpleAccordion({ tomare }: { tomare: TomareState }) {
 
                 <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
                     <Checkbox
-                        checked={tomare.checked
-                        }
+                        checked={checked[tomare.checked]}
                         onChange={handleCheck}
                         inputProps={{ 'aria-label': 'controlled' }}
                     />
