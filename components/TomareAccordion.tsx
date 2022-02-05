0c5b03d917990860eae7e39e7fc47485059efb48
asagaorino0 <xcnsn724@yahoo.co.jp>
2022-02-06 00:19:40 +0900

    PageA_tomare表示／非表示

--- components/TomareAccordion.tsx
+++ components/TomareAccordion.tsx
@@ -71,7 +71,7 @@ export default function SimpleAccordion({ tomare }: { tomare: TomareState }) {
             setExpanded(newExpanded ? panel : false);
         };
     // const [checked, setChecked] = React.useState([true, false]);
-    const [checked, setChecked] = React.useState(tomare.checked);
+    const [checked, setChecked] = React.useState<boolean>(tomare.checked);
 
 
     const handleChange2 = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -93,7 +93,7 @@ export default function SimpleAccordion({ tomare }: { tomare: TomareState }) {
 
                 <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
                     <Checkbox
-                        checked={tomare.checked}
+                        checked={checked}
                         onChange={handleCheck}
                         inputProps={{ 'aria-label': 'controlled' }}
                     />
