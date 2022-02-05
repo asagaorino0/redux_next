0a7020dff22760775eb6c37a6f591b23de51bcb0
asagaorino0 <xcnsn724@yahoo.co.jp>
2022-02-05 09:22:14 +0900

    PageA_tomare整理

--- components/TomareAccordion.tsx
+++ components/TomareAccordion.tsx
@@ -67,7 +67,7 @@ export default function SimpleAccordion({ tomare }: { tomare: TomareState }) {
                 </AccordionSummary>
                 <AccordionDetails>
                     <Typography>
-                        <div className="flex justify-between ...">
+                        <div className="grid grid-cols-2 gap-2 place-content-between h-48 ...">
                             <div>
                                 {`${tomare.img_befor}`.length !== 0 &&
                                     <img
