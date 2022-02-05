6523bc750a86c7f9e376befc4941f9dea0838f4d
asagaorino0 <xcnsn724@yahoo.co.jp>
2022-02-05 14:54:24 +0900

    PageA_img配置

--- components/TomareAccordion.tsx
+++ components/TomareAccordion.tsx
@@ -73,7 +73,7 @@ export default function SimpleAccordion({ tomare }: { tomare: TomareState }) {
                                 <img
                                     src={`${tomare.img_befor}`}
                                     alt=""
-                                    style={{ width: '100%', margin: '0' }}
+                                    style={{ width: '98%', margin: '0' }}
                                 />}
                             {user.o_befor_come !== 0 &&
                                 `${tomare.come_befor}`.length !== 0 && `${tomare.come_befor}`
@@ -84,7 +84,7 @@ export default function SimpleAccordion({ tomare }: { tomare: TomareState }) {
                                     <img
                                         src={`${tomare.img_after}`}
                                         alt=""
-                                        style={{ width: '100%', margin: '0' }}
+                                        style={{ width: '98%', margin: '0' }}
                                     />}
                                 {user.o_after_come !== 0 &&
                                     `${tomare.come_after}`.length !== 0 && `${tomare.come_after}`
