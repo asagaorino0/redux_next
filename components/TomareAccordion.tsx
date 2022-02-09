ae3886107868b66765ae138f80216b76d104e19d
asagaorino0 <xcnsn724@yahoo.co.jp>
2022-02-09 23:35:40 +0900

    PageA_fileの取り込み_tomareChenge

--- components/TomareAccordion.tsx
+++ components/TomareAccordion.tsx
@@ -125,8 +125,8 @@ export default function SimpleAccordion({ tomare }: { tomare: TomareState }) {
                             </div>
                             <div>
                                 {`${tomare.img_after}`.length !== 0 &&
-                                    // <img src={`${tomare.img_after}`} alt="" style={{ width: '98%', margin: '0' }}     />
-                                    <TomareFileChenge tomare={tomare.img_after} tomareId={tomare.tomareId} uid={tomare.uid} label='img_after' key={tomare.tomareId} />
+                                    <img src={`${tomare.img_after}`} alt="" style={{ width: '98%', margin: '0' }} />
+                                    // <TomareFileChenge tomare={tomare.img_after} tomareId={tomare.tomareId} uid={tomare.uid} label='img_after' key={tomare.tomareId} />
                                 }
                                 {`${tomare.img_after}`.length === 0 &&
                                     <TomareFileUpload tomare={tomare.img_after} tomareId={tomare.tomareId} uid={tomare.uid} label='img_after' key={tomare.tomareId} />
