39ee43672ba1a85585d99d57c14644555dec7919
asagaorino0 <xcnsn724@yahoo.co.jp>
2022-02-10 22:30:48 +0900

    PageA_fileの取り込み_tomareChenge

--- components/TomareFileUpload.tsx
+++ components/TomareFileUpload.tsx
@@ -52,14 +52,16 @@ const TomareFileUpload = ({ tomare, tomareId, uid, label }: { tomare: string, to
                         <Stack direction="row" alignItems="center" spacing={2}>
                             {`${kyFile}`.length === 0 &&
                                 <label htmlFor="icon-button-file">
-                                    <Input accept="image/*" id="icon-button-file" type="file" onChange={onFileInputChange} />
+                                    {/* <Input accept="image/*" id="icon-button-file" type="file" onChange={onFileInputChange} /> */}
+                                    <Input type="file" onChange={onFileInputChange} />
                                     <IconButton color="primary" aria-label="upload picture" component="span"><PhotoCamera /></IconButton>
                                 </label>
                             }
                             {`${kyFile}`.length !== 0 &&
                                 <div>
                                     <label htmlFor="icon-button-file">
-                                        <Input accept="image/*" id="icon-button-file" type="file" onChange={onFileInputChange} />
+                                        {/* <Input accept="image/*" id="icon-button-file" type="file" onChange={onFileInputChange} /> */}
+                                        <Input type="file" onChange={onFileInputChange} />
                                         <img src={kyFile} alt={name} />
                                     </label>
                                     <br />
@@ -78,7 +80,8 @@ const TomareFileUpload = ({ tomare, tomareId, uid, label }: { tomare: string, to
                             {`${kyFile}`.length !== 0 &&
                                 <div>
                                     <label htmlFor="icon-button-file">
-                                        <Input accept="image/*" id="icon-button-file" type="file" onChange={onFileInputChange} />
+                                        {/* <Input accept="image/*" id="icon-button-file" type="file" onChange={onFileInputChange} /> */}
+                                        <Input type="file" onChange={onFileInputChange} />
                                         <img src={kyFile} alt={name} />
                                     </label>
                                     <br />
