53062fa69abd3cda24e0f93b2bddb47bd8f2604b
asagaorino0 <xcnsn724@yahoo.co.jp>
2022-03-01 21:46:42 +0900

    履歴_CostomerAccordion変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux';
 import { useRouter } from 'next/router';
 import dynamic from 'next/dynamic';
 const App = dynamic(() => import('../src/App'), { ssr: false });
-// const Appppp = dynamic(() => import('../src/Appppp'), { ssr: false });
+const Appppp = dynamic(() => import('../src/Appppp'), { ssr: false });
 // const PagePay = dynamic(() => import('../src/PagePay'), { ssr: false });
 
 const Home: NextPage = () => {
@@ -33,7 +33,7 @@ const Home: NextPage = () => {
         <section className="h-screen w-4/5 max-w-5xl mx-auto flex items-center justify center flex-col">
           <Provider store={store}>
             <App />
-            {/* <Appppp /> */}
+            <Appppp />
           </Provider>
         </section>
 
