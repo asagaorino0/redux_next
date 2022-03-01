0ac692bc7cd09259ac83bb68e743a65393d589eb
asagaorino0 <xcnsn724@yahoo.co.jp>
2022-03-01 18:05:50 +0900

    履歴_CostomerAccordion変更

--- components/CustomerAccordion.tsx
+++ components/CustomerAccordion.tsx
@@ -13,15 +13,16 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Stars } from "./Star";
 import { BsStar } from "react-icons/bs";
 import styles from '../styles/Home.module.css';
-import { doc, setDoc } from 'firebase/firestore'
 import { db } from "../src/firebase";
-import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import { UserState } from "../src/types/user";
-
-import TomareFileUpload from '../components/TomareFileUpload';
-import TomareFileChenge from '../components/TomareFileChenge';
-
+import {
+    collection,
+    query,
+    where,
+    doc,
+    setDoc,
+    onSnapshot,
+} from 'firebase/firestore';
+import { useRouter } from 'next/router';
 const Accordion = styled((props: AccordionProps) => (
     <MuiAccordion disableGutters elevation={0} square {...props} />
 ))(({ theme }) => ({
@@ -60,26 +61,38 @@ const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
 
 export default function SimpleAccordion({ tomare }: { tomare: TomareState }) {
     const [expanded, setExpanded] = React.useState<string | false>('panel1');
-    // const [checked, setChecked] = React.useState(true);
-    const handleCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setChecked(event.target.checked);
-        // setChecked([event.target.checked, checked[1]]);
-        console.log(checked, `${tomare.tomareId}`)
-        setDoc(doc(db, 'users', `${tomare.uid}`, 'tomare', `${tomare.tomareId}`), {
-            checked: event.target.checked,
-        }, { merge: true })
-    };
     const handleStar = (e: number) => {
-        setDoc(doc(db, 'users', `${tomare.uid}`, 'tomare', `${tomare.tomareId}`), { star: e }, { merge: true })
+        setDoc(doc(db, 'users', `${tomare.uid}`, 'tomare', `${tomare.tomareId}`), { star: e }, { merge: true });
+        toHome()
     };
+    const amountSub = tomare.tanka * tomare.quantity
+    const amount = tomare.tanka * tomare.quantity + tomare.chip
+    // const apiYoyakuId = `${tomare.yoyakuId}${amount}`
+    const stQua = 100 + tomare.quantity * 1
+    const yoyakuId = `${stQua}${tomare.uid}${tomare.yoyakuUid}${tomare.tomareId}`
+    const handleChip = (e: number) => {
+        // setDoc(doc(db, 'users', `${tomare.uid}`, 'tomare', `${tomare.tomareId}`), { chip: e, chipUrl: `${process.env.STRIPE_SECRET_e}` }, { merge: true })
+    };
+    const handleChip500 = (e: number) => {
+        setDoc(doc(db, 'users', `${tomare.uid}`, 'tomare', `${tomare.tomareId}`), { chip: e, cusPay: amount * 1 + e, chipUrl: "shr_1KXTkfIeKRfM8LCeTmYH0csl", yoyakuId: `${yoyakuId}shr_1KXTkfIeKRfM8LCeTmYH0csl` }, { merge: true })
+        toHome()
+    };
+    const handleChip1000 = (e: number) => {
+        setDoc(doc(db, 'users', `${tomare.uid}`, 'tomare', `${tomare.tomareId}`), { chip: e, cusPay: amount + e, chipUrl: "shr_1KXaVyIeKRfM8LCeQ9dJPPvV", yoyakuId: `${yoyakuId}shr_1KXaVyIeKRfM8LCeQ9dJPPvV` }, { merge: true })
+        toHome()
+    };
+    // const [setPay] = useState<any>([]);
+    // const dispatch = useDispatch();
     const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
     const user = useSelector(selectUser);
     const handleChange =
         (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
             setExpanded(newExpanded ? panel : false);
         };
-    // const [checked, setChecked] = React.useState([true, false]);
-    const [checked, setChecked] = React.useState<boolean>(tomare.checked);
+    const router = useRouter();
+    const toHome = () => {
+        router.push('./');
+    };
     const date = new Date()
     const Y = date.getFullYear()
     const M = ("00" + (date.getMonth() + 1)).slice(-2)
@@ -91,100 +104,98 @@ export default function SimpleAccordion({ tomare }: { tomare: TomareState }) {
 
     const toStripe = () => {
         setDoc(doc(db, 'yoyakuPay', `${tomare.yoyakuId}`), {
-            tomareId: tomare.tomareId,
-            pay: +tomare.tanka * tomare.quantity,
-            yoyakuUid: tomare.yoyakuUid,
+            // pay: 1,
             yoyakuId: tomare.yoyakuId,
+            amount,
             uid: tomare.uid,
             star: tomare.star,
             chip: tomare.chip,
+            timestamp: now,
+            tomareId: tomare.tomareId,
+            yoyakuMenu: tomare.yoyakuMenu,
+            yoyakuUid: tomare.yoyakuUid,
             img_befor: tomare.img_befor,
             img_after: tomare.img_after,
-            yoyakuMenu: tomare.yoyakuMenu,
-            timestamp: now
         }, { merge: true })
+        toHome()
     };
-    // const quantity = `${tomare.quantity}`
+
+    // const fetchPay = async () => {
+    //     const p = query(collection(db, 'yoyakuPay'), where('yoyakuUid', '==', tomare.uid));
+    //     const snapshot = onSnapshot(p, (querySnapshot) => {
+    //         const payData = querySnapshot.docs.map(
+    //             (docP) => ({ ...docP.data() } as TomareState)
+    //         );
+    //         dispatch(addUser(payData));
+    //         setPay(payData);
+    //     });
+    // };
 
     return (
         <div>
-            <Accordion
-                expanded={expanded === 'panel1'}
-                onChange={handleChange('panel1')}
-            >
-                {/* <Checkbox checked={checked[tomare.checked]} onChange={handleChange2} /> */}
+            {tomare.pay === 0 &&
 
-                <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-                    {/* <Checkbox
-                        checked={checked}
-                        onChange={handleCheck}
-                        inputProps={{ 'aria-label': 'controlled' }}
-                    /> */}
+                <Accordion
+                    expanded={expanded === 'panel1'}
+                    onChange={handleChange('panel1')}
+                >
+                    {/* <Checkbox checked={checked[tomare.checked]} onChange={handleChange2} /> */}
 
-                    {tomare.pay === 0 &&
-                        <form action={`/api/checkout/${tomare.quantity}/setup`} method="POST">
-                            <section>
-                                <button type="submit" role="link" className={styles.card} onClick={() => toStripe()} >
-                                    {`${tomare.tanka * tomare.quantity}円${tomare.tanka}円
-                                    ×${+ tomare.quantity * 10} 分`}
-                                </button>
-                            </section>
-                        </form>
-                    }
-                    {tomare.receipt_url !== "" &&
-                        <section>
-                            <a href={tomare.receipt_url}  >領収書</a>
-                        </section>
-                    }
-                    <Typography className={styles.grid}>{tomare.tomareId}:{tomare.yoyakuMenu}
-                        <br />
-                        {tomare.star !== 0 &&
+                    <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
 
-                            <Stars star={tomare.star} starSize={16} textSize={12} />}
-
-                        {tomare.star === 0 &&
-                            <div>
-                                {/* <br /> */}
-                                <button onClick={(e) => handleStar(1)}><BsStar /></button>
-                                <button onClick={(e) => handleStar(2)}><BsStar /></button>
-                                <button onClick={(e) => handleStar(3)}><BsStar /></button>
-                                <button onClick={(e) => handleStar(4)}><BsStar /></button>
-                                <button onClick={(e) => handleStar(5)}><BsStar /></button>
-                                まだ評価されていません
-                                <br />
-                            </div>
-                        }
-                        {tomare.chip !== 0 &&
-                            `chip:${tomare.chip}`
-                        }
-                    </Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                        {/* <div className="flex justify-between ...">    */}
-                        <div className="flex justify-evenly ...">
-                            <div>
-                                {tomare.checked === true &&
+                        <Typography className={styles.grid}>{tomare.tomareId}:{tomare.yoyakuMenu}
+                            <br />
+                            {`${tomare.chip}`.toString() === 'undefined' &&
+                                <div className={styles.card} >
+                                    {`${amountSub}円`}
+                                    <br />
+                                    {`${tomare.tanka}円×${+ tomare.quantity * 10} 分`}
+                                </div>
+                            }
+                            {tomare.star !== 0 &&
+                                <Stars star={tomare.star} starSize={16} textSize={12} />}
+                            {tomare.star === 0 &&
+                                <div>
+                                    {/* <br /> */}
+                                    <button onClick={(e) => handleStar(1)}><BsStar /></button>
+                                    <button onClick={(e) => handleStar(2)}><BsStar /></button>
+                                    <button onClick={(e) => handleStar(3)}><BsStar /></button>
+                                    <button onClick={(e) => handleStar(4)}><BsStar /></button>
+                                    <button onClick={(e) => handleStar(5)}><BsStar /></button>
+                                    <br />
+                                    まだ評価されていません
+                                </div>
+                            }
+                            {tomare.chip !== 0 &&
+                                `chip:${tomare.chip}`
+                            }
+                            {tomare.receipt_url !== "" &&
+                                <section>
+                                    <a href={tomare.receipt_url}  >領収書</a>
+                                </section>
+                            }
+                        </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>
+                            {/* <div className="flex justify-between ...">    */}
+                            <div className="flex justify-evenly ...">
+                                <div>
                                     <img src={tomare.img_befor} alt="" />
-                                }
-                                {tomare.checked === true && user.o_befor_come !== 0 &&
-                                    `${tomare.come_befor}`
-                                }
-                            </div>
-                            <div>
-                                {tomare.checked === true &&
+                                    {/* {tomare.checked === true && user.o_befor_come !== 0 &&
+                                    `${tomare.come_befor}`} */}
+                                </div>
+                                <div>
                                     <img src={tomare.img_after} alt="" />
-                                }
-                                {tomare.checked === true && user.o_after_come !== 0 &&
-                                    `${tomare.come_after}`
-                                }
+                                    {/* {tomare.checked === true && user.o_after_come !== 0 &&
+                                    `${tomare.come_after}`} */}
+                                </div>
                             </div>
-                        </div>
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-
-        </div>
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+            }
+        </div >
     );
 }
 
