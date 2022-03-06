f9bbb4e865d91c304e986665304d1bfc21dea899
asagaorino0 <xcnsn724@yahoo.co.jp>
2022-03-06 09:46:38 +0900

    予約枠の変更_targetTomare復活_quantity _pageD1

--- src/PageD1.tsx
+++ src/PageD1.tsx
@@ -150,7 +150,7 @@ const PageD1 = () => {
                         return (
                             <div key={users.uid}>
                                 {
-                                    targetTomare.tomare
+                                    targetTomare.targetTomare
                                         // .filter((users: UsersState) => tomare.uid === users.uid)
                                         .filter((tomare: TomareState) => tomare.uid === users.uid)
                                         .map((tomare: TomareState) => {
