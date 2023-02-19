78848a0f7902e8c75bea695f654a46102bb8c013
asagaorino0 <xcnsn724@yahoo.co.jp>
2023-02-20 02:06:01 +0900

    inputColor_headerなし３

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+/**
+ * @type { import("next").NextConfig}
+ */
+const config = {
+  experimental: {
+    swcLoader: true,
+    swcMinify: true,
+    cpus: 4,
+  },
+};
+module.exports = config;
