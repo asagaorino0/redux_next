2738e117505b0b3fcebe5cdf7f178f9b8c5b9ce9
asagaorino0 <xcnsn724@yahoo.co.jp>
2023-02-20 02:00:16 +0900

    inputColor_headerなし２

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-      config.resolve.fallback.child_process = false;
-      config.resolve.fallback.net = false;
-      config.resolve.fallback.dns = false;
-      config.resolve.fallback.tls = false;
-    }
-    return config;
-  },
+}
 
-};
+module.exports = nextConfig
