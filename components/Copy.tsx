4304cdff12334e2cce5e3b45648f6b463aa71ed5
asagaorino0 <xcnsn724@yahoo.co.jp>
2023-02-25 13:27:35 +0900

    inputColor20230225_2

--- components/Copy.tsx
+++ components/Copy.tsx
@@ -1,110 +1,103 @@
 
-
-import { makeStyles } from '@mui/styles';
+import { styled } from '@mui/material/styles';
 import { useSelector } from 'react-redux';
+import MuiBox from '@mui/material/Box';
 import { selectColor } from '../src/features/colorSlice';
 
 export default function Copy() {
     const color = useSelector(selectColor);
-    const useStyles = makeStyles((theme: any) => ({
-        info_h2: {
-            position: 'relative',
-            padding: '0.4em',
-            // margin: '0.1em',     
-            background: `${color.base}`,
-            fontSize: '1.2em',
-            fontWeight: 'bold',
-            color: `${color.accent}`,
-            '&::before': {
-                position: 'absolute',
-                content: '""',
-                top: '100%',
-                left: 0,
-                border: 'none',
-                borderBottom: 'solid 15px transparent',
-                borderRight: `solid 20px${color.sub}`
-            }
-        },
-        border: {
-            padding: '6px',
-            fontSize: '1.2em',
-            fontWeight: 'bold',
-            color: `${color.accent}`,
-            background: `${color.base}`,
-            borderBottom: `solid 6px ${color.sub}`,/*下線*/
-        },
-        headline: {
-            fontSize: '1.2em',
-            fontWeight: 'bold',
-            position: 'relative',
-            background: `${color.base}`,
-            boxShadow: `0px 0px 0px 5px ${color.base}`,
-            border: 'dashed 2px white',
-            padding: '0.2em 0.5em',
-            color: `${color.accent}`,
-            '&::after': {
-                position: 'absolute',
-                content: '""',
-                left: '-8px',
-                top: '-8px',
-                borderWidth: '0 0 16px 16px',
-                borderStyle: 'solid',
-                borderColor: `#fff #fff ${color.sub}`,
-                boxShadow: '1px 1px 1px rgba(0, 0, 0, 0.15)',
-            }
-        },
-        masteTape: {
+    const BoxA = styled(MuiBox)(() => ({
+        position: 'relative',
+        padding: '0.4em',
+        background: `${color.base}`,
+        fontSize: '1.2em',
+        fontWeight: 'bold',
+        color: `${color.accent}`,
+        '&::before': {
             position: 'absolute',
-            top: '-0.75em',
-            background: `${color.sub}`,
-            filter: `opacity(55%)`,
-            padding: '0.25em 2em',
-            fontSize: '1.2em',
-            fontWeight: 'bold',
-            color: `${color.accent}`,
-            transform: 'rotate(-3deg)',
-            left: '3px',
+            content: '""',
+            top: '100%',
+            left: 0,
+            border: 'none',
+            borderBottom: 'solid 15px transparent',
+            borderRight: `solid 20px${color.sub}`
         },
-        masteBox: {
-            position: 'relative',
-            background: `repeating-linear-gradient(-45deg, ${color.base}, ${color.base} 10px, ${color.sub} 0, ${color.sub} 12px)`,
-            padding: '1em',
-        },
-        masteBox_p: {
-            fontSize: '1.2em',
-            fontWeight: 'bold',
-            color: `${color.accent}`,
-            padding: '15px 10px 10px 10px',
-            textAlign: 'left',
+    }));
+    const BoxB = styled(MuiBox)(() => ({
+        padding: '6px',
+        fontSize: '1.2em',
+        fontWeight: 'bold',
+        color: `${color.accent}`,
+        background: `${color.base}`,
+        borderBottom: `solid 6px ${color.sub}`
+    }));
+    const BoxC = styled(MuiBox)(() => ({
+        fontSize: '1.2em',
+        fontWeight: 'bold',
+        position: 'relative',
+        background: `${color.base}`,
+        boxShadow: `0px 0px 0px 5px ${color.base}`,
+        border: 'dashed 2px white',
+        padding: '0.2em 0.5em',
+        color: `${color.accent}`,
+        '&::after': {
+            position: 'absolute',
+            content: '""',
+            left: '-8px',
+            top: '-8px',
+            borderWidth: '0 0 16px 16px',
+            borderStyle: 'solid',
+            borderColor: `#fff #fff ${color.sub}`,
+            boxShadow: '1px 1px 1px rgba(0, 0, 0, 0.15)',
         }
     }));
-    const classes = useStyles()
+    const BoxMT = styled(MuiBox)(() => ({
+        background: `repeating-linear-gradient(-45deg, ${color.base}, ${color.base} 10px, ${color.sub} 0, ${color.sub} 12px)`,
+        filter: `opacity(55%)`,
+        padding: '0.25em 2em',
+        fontSize: '1.2em',
+        fontWeight: 'bold',
+        color: `${color.accent}`,
+        transform: 'rotate(-3deg)',
+        left: '3px',
+    }));
+    const MtBox = styled(MuiBox)(() => ({
+        position: 'relative',
+        background: `repeating-linear-gradient(-45deg, ${color.base}, ${color.base} 10px, ${color.sub} 0, ${color.sub} 12px)`,
+        padding: '1em',
+    }));
+    const MtBox_p = styled(MuiBox)(() => ({
+        fontSize: '1.2em',
+        fontWeight: 'bold',
+        color: `${color.accent}`,
+        padding: '15px 10px 10px 10px',
+        textAlign: 'left',
+    }));
     return (
         <>
-            <section className={classes.info_h2}>
-                <>
+            <section className="p-2">
+                <BoxA >
                     Hello!World
-                </>
+                </BoxA>
             </section>
             <br />
-            <br />
-            <section className={classes.border}>
-                <>
+            <section className="p-2">
+                <BoxB >
                     Hello!World
-                </>
+                </BoxB>
             </section>
             <br />
-            <br />
-            <section className={classes.headline}>
-                <>
+            <section className="p-2">
+                <BoxC >
                     Hello!World
-                </>
+                </BoxC>
             </section>
-            <br />
-            <br />
-            <section className={classes.masteBox}>
-                <span className={classes.masteTape}>　　</span>
-                <p className={classes.masteBox_p}>Hello!World</p>
+            <section className="p-5">
+                <BoxMT>
+                    <MtBox_p >
+                        Hello!World
+                    </MtBox_p>
+                </BoxMT>
             </section>
         </>
     );
