69f8fde0ef6b3b49490b96ca7cced65a40728a12
asagaorino0 <xcnsn724@yahoo.co.jp>
2023-02-28 23:59:24 +0900

    どうかな３

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,6 @@ const Home: NextPage = () => {
       <div className="pt-12">
         {/* <h1 className="text-3xl mb-4">{siteConfig.title}</h1> */}
         <div className="grid md:gap-6 mt-10 md:grid-cols-2 w-full my-12">
-          {/* Card */}
           <InputColor />
           <br />
           <Copy />
