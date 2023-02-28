ed8976069eb366baffc3acc5e1448e65fd0d398b
asagaorino0 <xcnsn724@yahoo.co.jp>
2023-03-01 07:42:14 +0900

    どうかな7

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,8 @@
+import { store } from '@/app/store';
 import type { NextPage } from 'next';
+import dynamic from 'next/dynamic';
+import React from 'react';
+import { Provider } from 'react-redux';
 import Copy from '../components/Copy';
 import InputColor from '../components/InputColor';
 import Layout from '../components/Layout';
@@ -7,16 +11,24 @@ import { siteConfig } from '../const/site.config';
 import App from '../src/App';
 
 const Home: NextPage = () => {
+  const App = dynamic(() => import('@/App'), { ssr: false });
   return (
     <Layout>
       <div className="pt-12">
-        {/* <h1 className="text-3xl mb-4">{siteConfig.title}</h1> */}
-        <div className="grid md:gap-6 mt-10 md:grid-cols-2 w-full my-12">
-          <InputColor />
-          <br />
-          <Copy />
-          <App />
-        </div>
+        <InputColor />
+        <br />
+        <Copy />
+        <React.StrictMode >
+          <Provider store={store}>
+            <div className="bg-blue-500 sticky top-0">
+              {/* <h1 className="text-3xl mb-4">{siteConfig.title}</h1> */}
+              <div className="grid md:gap-6 mt-10 md:grid-cols-2 w-full my-12">
+                <App />
+              </div>
+            </div>
+          </Provider>
+        </React.StrictMode>
+
       </div>
     </Layout>
   );
