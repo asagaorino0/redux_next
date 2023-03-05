bb0a6e50b965d8fc045ea2bd87092f51afaf95e4
asagaorino0 <xcnsn724@yahoo.co.jp>
2023-03-05 11:42:04 +0900

    どうかなLogin19

--- components/Login.tsx
+++ components/Login.tsx
@@ -38,74 +38,75 @@ export default function Login() {
     const dispatch = useDispatch();
     const user = useSelector(selectUser);
 
-    // useEffect(() => {
-    //     liff
-    //         .init({ liffId: process.env.NEXT_PUBLIC_REACT_APP_LIFF_ID as string })
-    //         .then(async () => {
-    //             if (liff.isLoggedIn()) {
-    //                 console.log('login status : [', true, ']');
-    //                 const profile = await liff.getProfile();
-    //                 console.log(
-    //                     '🚀 ~ file: Login.tsx ~ line 27 ~ liff.init ~ profile',
-    //                     profile
-    //                 );
-    //                 // const userId: string = profile.userId
-    //                 const displayName: string = profile.displayName;
-    //                 const displayicon: string | undefined = profile.pictureUrl;
-    //                 setName(profile.displayName);
-    //                 setUid(profile.userId);
-    //                 setName(displayName);
-    //                 setIcon(displayicon);
-    //                 setUid(profile.userId)
-    //                 await setLoginUser(
-    //                     user,
-    //                     profile.userId,
-    //                     displayName,
-    //                     displayicon,
-    //                 );
-    //                 dispatch(
-    //                     addUser({
-    //                         name: profile.displayName,
-    //                         uid: profile.userId,
-    //                         icon: profile.pictureUrl,
-    //                     })
-    //                 );
-    //             } else {
-    //                 console.log('login status : [', false, ']');
-    //             }
-    //         });
-    // }, []);
+    useEffect(() => {
+        liff
+            .init({ liffId: process.env.NEXT_PUBLIC_REACT_APP_LIFF_ID as string })
+            .then(async () => {
+                if (liff.isLoggedIn()) {
+                    console.log('login status : [', true, ']');
+                    const profile = await liff.getProfile();
+                    console.log(
+                        '🚀 ~ file: Login.tsx ~ line 27 ~ liff.init ~ profile',
+                        profile
+                    );
+                    // const userId: string = profile.userId
+                    const displayName: string = profile.displayName;
+                    const displayicon: string | undefined = profile.pictureUrl;
+                    setName(profile.displayName);
+                    setUid(profile.userId);
+                    setName(displayName);
+                    setIcon(displayicon);
+                    setUid(profile.userId)
+                    dispatch(
+                        addUser({
+                            name: profile.displayName,
+                            uid: profile.userId,
+                            icon: profile.pictureUrl,
+                        })
+                    );
+                    await setLoginUser(
+                        user,
+                        profile.userId,
+                        displayName,
+                        displayicon,
+                    );
+
+                } else {
+                    console.log('login status : [', false, ']');
+                }
+            });
+    }, []);
     const lineClick = () => {
         liff.init({ liffId: LINEID as string }).then(async () => {
-            if (!liff.isLoggedIn()) {
-                liff.login(); // ログインしていなければ最初にログインする
-            } else if (liff.isInClient()) {
-                console.log('hello world');
-                const profile = await liff.getProfile();
-                console.log(
-                    '🚀 ~ file: Login.tsx ~ line 57 ~ liff.init ~ profile',
-                    profile
-                );
-                const displayName: string = profile.displayName;
-                const displayicon: string | undefined = profile.pictureUrl;
-                setName(profile.displayName);
-                setUid(profile.userId);
-                setName(displayName);
-                setIcon(displayicon);
-                dispatch(
-                    addUser({
-                        name: profile.displayName,
-                        uid: profile.userId,
-                        icon: profile.pictureUrl,
-                    })
-                );
-                await setLoginUser(
-                    user,
-                    profile.userId,
-                    displayName,
-                    displayicon,
-                );
-            }
+            // if (!liff.isLoggedIn()) {
+            liff.login(); // ログインしていなければ最初にログインする
+            // } else if (liff.isInClient()) {
+            //     console.log('hello world');
+            //     const profile = await liff.getProfile();
+            //     console.log(
+            //         '🚀 ~ file: Login.tsx ~ line 57 ~ liff.init ~ profile',
+            //         profile
+            //     );
+            //     const displayName: string = profile.displayName;
+            //     const displayicon: string | undefined = profile.pictureUrl;
+            //     setName(profile.displayName);
+            //     setUid(profile.userId);
+            //     setName(displayName);
+            //     setIcon(displayicon);
+            //     dispatch(
+            //         addUser({
+            //             name: profile.displayName,
+            //             uid: profile.userId,
+            //             icon: profile.pictureUrl,
+            //         })
+            //     );
+            //     await setLoginUser(
+            //         user,
+            //         profile.userId,
+            //         displayName,
+            //         displayicon,
+            //     );
+            // }
         })
     };
     return (
